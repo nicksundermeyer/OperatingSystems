ed6a1660d6c04896b91672888517f121165d80f7
nicksundermeyer <nicksundermeyer@gmail.com>
2018-02-14 17:23:18 -0700

    added comments

--- Program1/Scheduler.h
+++ Program1/Scheduler.h
@@ -23,7 +23,13 @@
 
 class Scheduler {
 public:
-    // constructor
+    /*
+     *  Constructor - set up list of processes and other parameters
+     * 
+     * @file_name - name of the file to read processes from
+     * @block_duration - duration of blocking for all processes
+     * @time_slice - time quantum for round-robin scheduling
+     */
     Scheduler(std::string file_name, uint32_t block_duration, uint32_t time_slice);
     
     // destructor
@@ -41,29 +47,37 @@ public:
     // move assignment operator
     Scheduler operator=(const Scheduler &&orig) = delete;
     
+    /*
+     * simulate_RR - simulate round-robin scheduling on processes in the 'processes' vector
+     */
     void simulate_RR();
+    
+    /*
+     * simulate_SPN - simulate shortest-process-next scheduling on processes in the 'processes' vector
+     */
     void simulate_SPN();
     
 private:
     
     struct Process{
-        std::string name;
-        uint32_t arrival_time;
-        uint32_t total_time;
-        uint32_t processed_time;
-        uint32_t block_interval;
-        uint32_t blocked_time;
+        std::string name; // name of process
+        uint32_t arrival_time; // at what time the process arrives
+        uint32_t total_time; // total time needed to complete process
+        uint32_t processed_time; // total time processed
+        uint32_t block_interval; // time interval before process blocks
+        uint32_t blocked_time; // time in blocked list
         uint32_t turnaround_time;
     };
     
-    std::string file_name;
-    uint32_t block_duration;
-    uint32_t time_slice;
+    std::string file_name; // name of file to read
+    uint32_t block_duration; // duration of blocking for all processes
+    uint32_t time_slice; // time quantum for round-robin
     
-    std::vector<Process> processes;
-    std::vector<Process> ready;
-    std::vector<Process> blocked;
+    std::vector<Process> processes; // processes to simulate
+    std::vector<Process> ready; // currently ready processes
+    std::vector<Process> blocked; // currently blocked processes
     
+    // trivial function to print out a vector
     void printVector(std::vector<Process> v);
 };
 
--- Program1/main.cpp
+++ Program1/main.cpp
@@ -20,11 +20,10 @@
 
 int main(int argc, char** argv) {
     
-    Scheduler s("program1_sample1.txt", 20, 10);
-    //s.simulate_SPN();
+//    Scheduler s("program1_sample1.txt", 20, 10);
+    Scheduler s(argv[1], 20, 10);
+    s.simulate_SPN();
     s.simulate_RR();
-    
-    
     
     return 0;
 }
